c2dd775077e57b71943660d5f9acab869f21c311
Emery Tate <tateejt@gmail.com>
2019-12-04 02:59:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,25 +1,21 @@
-# require '../../config/environment'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  set :views, Proc.new { File.join(root, "../views/") }
-  
   get '/' do
     erb :super_hero
   end
 
   post '/teams' do
-    
     @team_name = params[:team][:name]
     @team_motto = params[:team][:motto]
     @hero_name = []
     @hero_power = []
     @hero_bio = []
     @team_members = params[:team][:members]
-    @team_members.each do |hero|
-      @hero_name << hero[:name]
-      @hero_power << hero[:power]
-      @hero_bio << hero[:bio]
+    @team_members.each do |hero, attrib|
+      @hero_name << attrib[:name]
+      @hero_power << attrib[:power]
+      @hero_bio << attrib[:bio]
     end
 
     erb :team
